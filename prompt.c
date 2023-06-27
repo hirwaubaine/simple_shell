809669ddb521e1f7b86d35b48341869d3cf9add3
hirwaubaine <hirwaubaine@gmail.com>
2023-06-27 00:07:19 -0700

    ALX task

--- prompt.c
+++ prompt.c
@@ -1,17 +1,15 @@
 #include "shell.h"
 #define MAX_COMMAND 100
-
 /**
  * prompt - Prompt for the shell built
  * @agv: array of argument
  * @environ: environment variables
  * Rerurn: always 0
  */
-
 void prompt(char **agv, char **environ)
 {
 	char *strg = NULL;
-	int i, status, j;
+	int i = 0, status, j = 0;
 	size_t n = 0;
 	ssize_t n_char;
 	char *argv[MAX_COMMAND];
@@ -19,6 +17,7 @@ void prompt(char **agv, char **environ)
 
 	while (1)
 	{
+		if (isatty(STDIN_FILENO))
 		printf("cisfun$ ");
 
 		n_char = getline(&strg, &n, stdin);
@@ -27,20 +26,15 @@ void prompt(char **agv, char **environ)
 			free(strg);
 			exit(EXIT_FAILURE);
 		}
-		i = 0;
 		while (strg[i])
 		{
 			if (strg[i] == '\n')
 				strg[i] = 0;
 			i++;
 		}
-		j = 0;
 		argv[j] = strtok(strg, " ");
 		while (strg[j])
-		{
-			j++;
-			argv[j] = strtok(NULL, " ");
-		}
+			argv[++j] = strtok(NULL, " ");
 		child_pid = fork();
 		if (child_pid == -1)
 		{
