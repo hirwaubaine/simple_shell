3f181a0712c51500f9b90439894b9b94baffe039
hirwaubaine <hirwaubaine@gmail.com>
2023-06-26 23:35:06 -0700

    ALX task

--- shell.c
+++ shell.c
@@ -1,5 +1,14 @@
 #include "shell.h"
 
+/**
+ * main - Main shell built
+ * Description: a program that build a shell from scratch
+ * @agc: number of arguments
+ * @agv: array of  arguments
+ * @environ: environment variables
+ * Return: 0
+ */
+
 int main(int agc, char **agv, char **environ)
 {
 	if (agc == 1)
